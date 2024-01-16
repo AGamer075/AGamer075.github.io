4c5750064516f088992a51216a96f3f6584e0588
AGamer075 <84387245+AGamer075@users.noreply.github.com>
2024-01-16 21:21:54 +0500

    something maybe

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.js";
-import { OrbitControls } from "./vendor/OrbitControls.js";
+import { OrbitControls } from "https://agamer075.github.io/vendor/OrbitControls.js";
 //Scene
 const scene = new THREE.Scene()
 
