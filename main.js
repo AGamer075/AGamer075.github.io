712cbe04e1b7545122bf6732de618db3cecc7e0a
AGamer075 <84387245+AGamer075@users.noreply.github.com>
2024-01-17 22:08:52 +0500

    ge

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/+esm";
 //import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js'
 
 //Scene
