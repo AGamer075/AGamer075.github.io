6f46f14712dcfe000a71af2375d94f0089f4d39a
AGamer075 <84387245+AGamer075@users.noreply.github.com>
2024-01-17 20:36:42 +0500

    Maybe

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from "./vendor/three.module.js";
-// import { OrbitControls  } from "./vendor/OrbitControls.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 //Scene
 const scene = new THREE.Scene()
 
@@ -51,7 +51,7 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 //Controls
-// const controls = new OrbitControls(camera, canvasa)
+const controls = new OrbitControls(camera, canvasa)
 // controls.enableZoom = false
 // controls.enableDamping = true
 // controls.enablePan = false
